11b835bf7262e8a2d2b4fc9f411bf83e29cfd281
Twaha-Rahman <mahi6703890@gmail.com>
2020-04-18 20:44:44 +0600

    Updates

---
+++ precache-manifest.d650f37b4ebcf55ba079de22cb70d594.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a171271dac0ff262d21fcd8a901ea3c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2753820c3b58186874f8",
+    "url": "/static/css/main.472d70cb.chunk.css"
+  },
+  {
+    "revision": "ae58f10bab8ae213b00d",
+    "url": "/static/js/2.38df237d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.38df237d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2753820c3b58186874f8",
+    "url": "/static/js/main.d045dc81.chunk.js"
+  },
+  {
+    "revision": "817d2bf81990a1d39ee2",
+    "url": "/static/js/runtime-main.6537f79a.js"
+  },
+  {
+    "revision": "85d339d916479f729938d2911b85bf1f",
+    "url": "/static/media/Lato-Bold.85d339d9.ttf"
+  },
+  {
+    "revision": "2d36b1a925432bae7f3c53a340868c6e",
+    "url": "/static/media/Lato-Regular.2d36b1a9.ttf"
+  },
+  {
+    "revision": "30df16de993c50c9264842664e4f0957",
+    "url": "/static/media/iconfinder___wash_hand_soap_5925226.30df16de.png"
+  },
+  {
+    "revision": "c78864e49276ca35572ecfa3550c7fed",
+    "url": "/static/media/iconfinder___wash_hand_soap_5925226.c78864e4.webp"
+  },
+  {
+    "revision": "1d5ca1d4ee2c7852b3a015ea8005af73",
+    "url": "/static/media/iconfinder_facial_mask_coronavirus_5964544.1d5ca1d4.webp"
+  },
+  {
+    "revision": "53a11615d0d7b1e7b26a244cb281ed27",
+    "url": "/static/media/iconfinder_keep_distance_social_1_5964546.53a11615.png"
+  },
+  {
+    "revision": "d7df300cd85a57a5bd19858397847d2b",
+    "url": "/static/media/iconfinder_keep_distance_social_1_5964546.d7df300c.webp"
+  }
+]);
