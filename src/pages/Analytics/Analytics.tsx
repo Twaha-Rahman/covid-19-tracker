a917ea373f08f8f6704f8cb8a10075fa1ee5cedd
Twaha-Rahman <mahi6703890@gmail.com>
2020-04-24 11:15:44 +0600

    Added loader in Analytics page while Loading

--- src/pages/Analytics/Analytics.tsx
+++ src/pages/Analytics/Analytics.tsx
@@ -21,8 +21,75 @@ class Analytics extends React.Component<IAnalytics, {}> {
   }
 
   public render() {
-    if (!this.props.store.addGlobalData.cases) {
-      return <Loading />;
+    let JSXTorender;
+
+    JSXTorender = <Loading />;
+    if (this.props.location.pathname === '/analytics/') {
+      if (this.props.store.addGlobalData.cases) {
+        JSXTorender = (
+          <main>
+            <div className="info-box-container">
+              <InfoBox
+                title={'Total Affected'}
+                number={this.props.store.addGlobalData.cases}
+                bcackgroundColor={'var(--custom-yellow)'}
+              />
+              <InfoBox
+                title={'Deaths Today'}
+                number={this.props.store.addGlobalData.todayDeaths}
+                bcackgroundColor={'var(--custom-red)'}
+              />
+              <InfoBox
+                title={'Affected Today'}
+                number={this.props.store.addGlobalData.todayCases}
+                bcackgroundColor={'var(--custom-yellow)'}
+              />
+              <InfoBox
+                title={'Total Deaths'}
+                number={this.props.store.addGlobalData.deaths}
+                bcackgroundColor={'var(--custom-red)'}
+              />
+              <InfoBox
+                title={'Recovered'}
+                number={this.props.store.addGlobalData.recovered}
+                bcackgroundColor={'var(--custom-green)'}
+              />
+              <InfoBox
+                title={'Active'}
+                number={this.props.store.addGlobalData.active}
+                bcackgroundColor={'var(--custom-blue)'}
+              />
+              <InfoBox
+                title={'Critical Cases'}
+                number={this.props.store.addGlobalData.critical}
+                bcackgroundColor={'var(--custom-purple)'}
+              />
+              <InfoBox
+                title={'Affected Countries'}
+                number={this.props.store.addGlobalData.affectedCountries}
+                bcackgroundColor={'var(--custom-red)'}
+              />
+
+              <InfoBox
+                title={'Persons Tested'}
+                number={this.props.store.addGlobalData.tests}
+                bcackgroundColor={'var(--custom-yellow)'}
+              />
+              <InfoBox
+                title={'Persons Tested Per Million People'}
+                number={this.props.store.addGlobalData.testsPerOneMillion}
+                bcackgroundColor={'var(--custom-yellow)'}
+              />
+            </div>
+          </main>
+        );
+      }
+    }
+
+    if (this.props.location.pathname === '/analytics/country') {
+      if (this.props.store.addCountryData.cases) {
+        JSXTorender = <div>lol</div>;
+      }
     }
 
     return (
@@ -35,61 +102,7 @@ class Analytics extends React.Component<IAnalytics, {}> {
           <h2>Statistics</h2>
           <Switch names={['Global', 'Country']} />
         </header>
-        <main>
-          <div className="info-box-container">
-            <InfoBox
-              title={'Total Affected'}
-              number={this.props.store.addGlobalData.cases}
-              bcackgroundColor={'var(--custom-yellow)'}
-            />
-            <InfoBox
-              title={'Deaths Today'}
-              number={this.props.store.addGlobalData.todayDeaths}
-              bcackgroundColor={'var(--custom-red)'}
-            />
-            <InfoBox
-              title={'Affected Today'}
-              number={this.props.store.addGlobalData.todayCases}
-              bcackgroundColor={'var(--custom-yellow)'}
-            />
-            <InfoBox
-              title={'Total Deaths'}
-              number={this.props.store.addGlobalData.deaths}
-              bcackgroundColor={'var(--custom-red)'}
-            />
-            <InfoBox
-              title={'Recovered'}
-              number={this.props.store.addGlobalData.recovered}
-              bcackgroundColor={'var(--custom-green)'}
-            />
-            <InfoBox
-              title={'Active'}
-              number={this.props.store.addGlobalData.active}
-              bcackgroundColor={'var(--custom-blue)'}
-            />
-            <InfoBox
-              title={'Critical Cases'}
-              number={this.props.store.addGlobalData.critical}
-              bcackgroundColor={'var(--custom-purple)'}
-            />
-            <InfoBox
-              title={'Affected Countries'}
-              number={this.props.store.addGlobalData.affectedCountries}
-              bcackgroundColor={'var(--custom-red)'}
-            />
-
-            <InfoBox
-              title={'Persons Tested'}
-              number={this.props.store.addGlobalData.tests}
-              bcackgroundColor={'var(--custom-yellow)'}
-            />
-            <InfoBox
-              title={'Persons Tested Per Million People'}
-              number={this.props.store.addGlobalData.testsPerOneMillion}
-              bcackgroundColor={'var(--custom-yellow)'}
-            />
-          </div>
-        </main>
+        {JSXTorender}
       </>
     );
   }
